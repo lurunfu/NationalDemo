5fb8a7fd2033e4d4eb6407dd4feb64764d9d9156
lulu <lulu@qq.com>
2019-11-28 09:41:43 +0800

    first commit

---
+++ js/1.js
@@ -0,0 +1,25 @@
+var now=1;
+var max=3;
+function show(op){
+				if(Number(op)){
+							now=parseInt(op);
+							clearTimeout(myTime);
+							  }
+for(var i=1;i<=max; i++){
+						if(i==now){
+									document.getElementById("lunbo"+now).style.display="block";
+									document.getElementById("d"+now).className="d1";
+									
+								}else{
+									document.getElementById("lunbo"+i).style.display="none";
+									document.getElementById("d"+i).className="d2";
+								}
+							}
+							now++;
+							if(now>max){
+								now=1;
+							}
+							
+							myTime=setTimeout("show()",3000);
+}
+
